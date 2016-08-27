4e18312256abd2fb34e8150a238af67badf40fbc
Tristan Smith <tristan@shortcord.com>
2016-08-27 12:03:55 -0400

    Added Documentation

---
+++ Docs/data/Navigation.js
@@ -0,0 +1,589 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.navigationData = [
+			
+
+	{
+		id: 'Discore',
+		text: 'Discore',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/Discore' },
+		children: [
+			
+ 
+
+		{
+			id: 'Discore.ChannelEventArgs',
+			text: 'ChannelEventArgs',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.ChannelEventArgs' }
+        },	
+		{
+			id: 'Discore.DiscordApiCache',
+			text: 'DiscordApiCache',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordApiCache' }
+        },	
+		{
+			id: 'Discore.DiscordApiCacheHelper',
+			text: 'DiscordApiCacheHelper',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordApiCacheHelper' }
+        },	
+		{
+			id: 'Discore.DiscordApiCacheHelperException',
+			text: 'DiscordApiCacheHelperException',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordApiCacheHelperException' }
+        },	
+		{
+			id: 'Discore.DiscordApiData',
+			text: 'DiscordApiData',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordApiData' }
+        },	
+		{
+			id: 'Discore.DiscordApiDataType',
+			text: 'DiscordApiDataType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/Discore.DiscordApiDataType' }
+        },	
+		{
+			id: 'Discore.DiscordApiInfoCache',
+			text: 'DiscordApiInfoCache',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.DiscordApiInfoCache' }
+        },	
+		{
+			id: 'Discore.DiscordAttachment',
+			text: 'DiscordAttachment',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordAttachment' }
+        },	
+		{
+			id: 'Discore.DiscordChannel',
+			text: 'DiscordChannel',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordChannel' }
+        },	
+		{
+			id: 'Discore.DiscordChannelType',
+			text: 'DiscordChannelType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/Discore.DiscordChannelType' }
+        },	
+		{
+			id: 'Discore.DiscordClient',
+			text: 'DiscordClient',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordClient' }
+        },	
+		{
+			id: 'Discore.DiscordColor',
+			text: 'DiscordColor',
+			icon: './assets/icons/struct_public.png',
+			a_attr: { href : '#!type/Discore.DiscordColor' }
+        },	
+		{
+			id: 'Discore.DiscordDMChannel',
+			text: 'DiscordDMChannel',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordDMChannel' }
+        },	
+		{
+			id: 'Discore.DiscordEmbed',
+			text: 'DiscordEmbed',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordEmbed' }
+        },	
+		{
+			id: 'Discore.DiscordEmbedProvider',
+			text: 'DiscordEmbedProvider',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordEmbedProvider' }
+        },	
+		{
+			id: 'Discore.DiscordEmbedThumbnail',
+			text: 'DiscordEmbedThumbnail',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordEmbedThumbnail' }
+        },	
+		{
+			id: 'Discore.DiscordEmoji',
+			text: 'DiscordEmoji',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordEmoji' }
+        },	
+		{
+			id: 'Discore.DiscordGame',
+			text: 'DiscordGame',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordGame' }
+        },	
+		{
+			id: 'Discore.DiscordGameType',
+			text: 'DiscordGameType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/Discore.DiscordGameType' }
+        },	
+		{
+			id: 'Discore.DiscordGuild',
+			text: 'DiscordGuild',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordGuild' }
+        },	
+		{
+			id: 'Discore.DiscordGuildChannel',
+			text: 'DiscordGuildChannel',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordGuildChannel' }
+        },	
+		{
+			id: 'Discore.DiscordGuildChannelType',
+			text: 'DiscordGuildChannelType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/Discore.DiscordGuildChannelType' }
+        },	
+		{
+			id: 'Discore.DiscordGuildEmbed',
+			text: 'DiscordGuildEmbed',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordGuildEmbed' }
+        },	
+		{
+			id: 'Discore.DiscordGuildMember',
+			text: 'DiscordGuildMember',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordGuildMember' }
+        },	
+		{
+			id: 'Discore.DiscordIntegration',
+			text: 'DiscordIntegration',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordIntegration' }
+        },	
+		{
+			id: 'Discore.DiscordIntegrationAccount',
+			text: 'DiscordIntegrationAccount',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordIntegrationAccount' }
+        },	
+		{
+			id: 'Discore.DiscordLogEventArgs',
+			text: 'DiscordLogEventArgs',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordLogEventArgs' }
+        },	
+		{
+			id: 'Discore.DiscordLogger',
+			text: 'DiscordLogger',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordLogger' }
+        },	
+		{
+			id: 'Discore.DiscordLoggerFilter',
+			text: 'DiscordLoggerFilter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordLoggerFilter' }
+        },	
+		{
+			id: 'Discore.DiscordLogLine',
+			text: 'DiscordLogLine',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordLogLine' }
+        },	
+		{
+			id: 'Discore.DiscordLogType',
+			text: 'DiscordLogType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/Discore.DiscordLogType' }
+        },	
+		{
+			id: 'Discore.DiscordMessage',
+			text: 'DiscordMessage',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordMessage' }
+        },	
+		{
+			id: 'Discore.DiscordOverwrite',
+			text: 'DiscordOverwrite',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordOverwrite' }
+        },	
+		{
+			id: 'Discore.DiscordOverwriteType',
+			text: 'DiscordOverwriteType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/Discore.DiscordOverwriteType' }
+        },	
+		{
+			id: 'Discore.DiscordPermission',
+			text: 'DiscordPermission',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/Discore.DiscordPermission' }
+        },	
+		{
+			id: 'Discore.DiscordPermissionException',
+			text: 'DiscordPermissionException',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordPermissionException' }
+        },	
+		{
+			id: 'Discore.DiscordRole',
+			text: 'DiscordRole',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordRole' }
+        },	
+		{
+			id: 'Discore.DiscordUnavailableGuild',
+			text: 'DiscordUnavailableGuild',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordUnavailableGuild' }
+        },	
+		{
+			id: 'Discore.DiscordUser',
+			text: 'DiscordUser',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordUser' }
+        },	
+		{
+			id: 'Discore.DiscordUserGuild',
+			text: 'DiscordUserGuild',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscordUserGuild' }
+        },	
+		{
+			id: 'Discore.DiscordUserStatus',
+			text: 'DiscordUserStatus',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/Discore.DiscordUserStatus' }
+        },	
+		{
+			id: 'Discore.DiscoreException',
+			text: 'DiscoreException',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscoreException' }
+        },	
+		{
+			id: 'Discore.DiscoreSocketException',
+			text: 'DiscoreSocketException',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.DiscoreSocketException' }
+        },	
+		{
+			id: 'Discore.ExceptionDispathEventArgs',
+			text: 'ExceptionDispathEventArgs',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.ExceptionDispathEventArgs' }
+        },	
+		{
+			id: 'Discore.GuildEventArgs',
+			text: 'GuildEventArgs',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.GuildEventArgs' }
+        },	
+		{
+			id: 'Discore.GuildMemberEventArgs',
+			text: 'GuildMemberEventArgs',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.GuildMemberEventArgs' }
+        },	
+		{
+			id: 'Discore.GuildRoleEventArgs',
+			text: 'GuildRoleEventArgs',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.GuildRoleEventArgs' }
+        },	
+		{
+			id: 'Discore.GuildUserEventArgs',
+			text: 'GuildUserEventArgs',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.GuildUserEventArgs' }
+        },	
+		{
+			id: 'Discore.ICacheable',
+			text: 'ICacheable',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#!type/Discore.ICacheable' }
+        },	
+		{
+			id: 'Discore.ICacheContainer',
+			text: 'ICacheContainer',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#!type/Discore.ICacheContainer' }
+        },	
+		{
+			id: 'Discore.IDiscordClient',
+			text: 'IDiscordClient',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#!type/Discore.IDiscordClient' }
+        },	
+		{
+			id: 'Discore.IDiscordObject',
+			text: 'IDiscordObject',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#!type/Discore.IDiscordObject' }
+        },	
+		{
+			id: 'Discore.IntegrationEventArgs',
+			text: 'IntegrationEventArgs',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.IntegrationEventArgs' }
+        },	
+		{
+			id: 'Discore.MessageEventArgs',
+			text: 'MessageEventArgs',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.MessageEventArgs' }
+        },	
+		{
+			id: 'Discore.TypingStartEventArgs',
+			text: 'TypingStartEventArgs',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.TypingStartEventArgs' }
+        },	
+		{
+			id: 'Discore.VoiceClientEventArgs',
+			text: 'VoiceClientEventArgs',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.VoiceClientEventArgs' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'Discore.Audio',
+		text: 'Discore.Audio',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/Discore.Audio' },
+		children: [
+			
+ 
+
+		{
+			id: 'Discore.Audio.CircularBuffer',
+			text: 'CircularBuffer',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Audio.CircularBuffer' }
+        },	
+		{
+			id: 'Discore.Audio.DiscordVoiceClient',
+			text: 'DiscordVoiceClient',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.Audio.DiscordVoiceClient' }
+        },	
+		{
+			id: 'Discore.Audio.DiscordVoiceRegion',
+			text: 'DiscordVoiceRegion',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.Audio.DiscordVoiceRegion' }
+        },	
+		{
+			id: 'Discore.Audio.DiscordVoiceState',
+			text: 'DiscordVoiceState',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.Audio.DiscordVoiceState' }
+        },	
+		{
+			id: 'Discore.Audio.IOpusConverter',
+			text: 'IOpusConverter',
+			icon: './assets/icons/interface_internal.png',
+			a_attr: { href : '#!type/Discore.Audio.IOpusConverter' }
+        },	
+		{
+			id: 'Discore.Audio.IPDiscoveryEventArgs',
+			text: 'IPDiscoveryEventArgs',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Audio.IPDiscoveryEventArgs' }
+        },	
+		{
+			id: 'Discore.Audio.OpusApplication',
+			text: 'OpusApplication',
+			icon: './assets/icons/enum_internal.png',
+			a_attr: { href : '#!type/Discore.Audio.OpusApplication' }
+        },	
+		{
+			id: 'Discore.Audio.OpusConverter',
+			text: 'OpusConverter',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Audio.OpusConverter' }
+        },	
+		{
+			id: 'Discore.Audio.OpusCtl',
+			text: 'OpusCtl',
+			icon: './assets/icons/enum_internal.png',
+			a_attr: { href : '#!type/Discore.Audio.OpusCtl' }
+        },	
+		{
+			id: 'Discore.Audio.OpusEncoder',
+			text: 'OpusEncoder',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Audio.OpusEncoder' }
+        },	
+		{
+			id: 'Discore.Audio.OpusError',
+			text: 'OpusError',
+			icon: './assets/icons/enum_internal.png',
+			a_attr: { href : '#!type/Discore.Audio.OpusError' }
+        },	
+		{
+			id: 'Discore.Audio.VoiceBuffer',
+			text: 'VoiceBuffer',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Audio.VoiceBuffer' }
+        },	
+		{
+			id: 'Discore.Audio.VoiceSocket',
+			text: 'VoiceSocket',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Audio.VoiceSocket' }
+        },	
+		{
+			id: 'Discore.Audio.VoiceSocketOPCode',
+			text: 'VoiceSocketOPCode',
+			icon: './assets/icons/enum_internal.png',
+			a_attr: { href : '#!type/Discore.Audio.VoiceSocketOPCode' }
+        },	
+		{
+			id: 'Discore.Audio.VoiceUDPSocket',
+			text: 'VoiceUDPSocket',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Audio.VoiceUDPSocket' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'Discore.Net',
+		text: 'Discore.Net',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/Discore.Net' },
+		children: [
+			
+ 
+
+		{
+			id: 'Discore.Net.DiscordClientWebSocket',
+			text: 'DiscordClientWebSocket',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Net.DiscordClientWebSocket' }
+        },	
+		{
+			id: 'Discore.Net.DiscordGatewayException',
+			text: 'DiscordGatewayException',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.Net.DiscordGatewayException' }
+        },	
+		{
+			id: 'Discore.Net.DiscordGuildChannelModifyParams',
+			text: 'DiscordGuildChannelModifyParams',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.Net.DiscordGuildChannelModifyParams' }
+        },	
+		{
+			id: 'Discore.Net.DiscordMessageGetStrategy',
+			text: 'DiscordMessageGetStrategy',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/Discore.Net.DiscordMessageGetStrategy' }
+        },	
+		{
+			id: 'Discore.Net.DiscordRestClientException',
+			text: 'DiscordRestClientException',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/Discore.Net.DiscordRestClientException' }
+        },	
+		{
+			id: 'Discore.Net.GatewayDisconnectCode',
+			text: 'GatewayDisconnectCode',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/Discore.Net.GatewayDisconnectCode' }
+        },	
+		{
+			id: 'Discore.Net.GatewayOPCode',
+			text: 'GatewayOPCode',
+			icon: './assets/icons/enum_internal.png',
+			a_attr: { href : '#!type/Discore.Net.GatewayOPCode' }
+        },	
+		{
+			id: 'Discore.Net.GatewaySocket',
+			text: 'GatewaySocket',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Net.GatewaySocket' }
+        },	
+		{
+			id: 'Discore.Net.IDiscordGateway',
+			text: 'IDiscordGateway',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#!type/Discore.Net.IDiscordGateway' }
+        },	
+		{
+			id: 'Discore.Net.IDiscordRestChannelsService',
+			text: 'IDiscordRestChannelsService',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#!type/Discore.Net.IDiscordRestChannelsService' }
+        },	
+		{
+			id: 'Discore.Net.IDiscordRestClient',
+			text: 'IDiscordRestClient',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#!type/Discore.Net.IDiscordRestClient' }
+        },	
+		{
+			id: 'Discore.Net.IDiscordRestMessagesService',
+			text: 'IDiscordRestMessagesService',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#!type/Discore.Net.IDiscordRestMessagesService' }
+        },	
+		{
+			id: 'Discore.Net.RestClient',
+			text: 'RestClient',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Net.RestClient' }
+        },	
+		{
+			id: 'Discore.Net.RestClientChannelsService',
+			text: 'RestClientChannelsService',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Net.RestClientChannelsService' }
+        },	
+		{
+			id: 'Discore.Net.RestClientInternalService',
+			text: 'RestClientInternalService',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Net.RestClientInternalService' }
+        },	
+		{
+			id: 'Discore.Net.RestClientMessagesService',
+			text: 'RestClientMessagesService',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Net.RestClientMessagesService' }
+        },	
+		{
+			id: 'Discore.Net.RestClientRateLimitManager',
+			text: 'RestClientRateLimitManager',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Net.RestClientRateLimitManager' }
+        },	
+		{
+			id: 'Discore.Net.RestClientService',
+			text: 'RestClientService',
+			icon: './assets/icons/class_internal.png',
+			a_attr: { href : '#!type/Discore.Net.RestClientService' }
+        },	
+		{
+			id: 'Discore.Net.RestErrorCode',
+			text: 'RestErrorCode',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/Discore.Net.RestErrorCode' }
+        },	
+
+
+		]
+	},
+
+
+	];
---
+++ Docs/data/Project.js
@@ -0,0 +1,15 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.projectData = {
+	name: 'Discore',
+	homepage: 'https://bundledsticksinkorperated.github.io/Discore/',
+	version: '1.0.0.0',
+	author: 'Bundled Sticks Inkorperated',
+	authorHomepage: 'https://bundledsticksinkorperated.github.io/',
+	hasLogo: false,
+	targetFxs: [
+		".NET Core 1.0",
+	]
+}
